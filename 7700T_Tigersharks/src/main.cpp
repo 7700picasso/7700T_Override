60d5fa98c688b9b140d728e6db483ebda3d1e0c7
7700R <94393717+7700picasso@users.noreply.github.com>
2026-08-18 16:41:56 -0700

    1st update

---
+++ 7700T_Tigersharks/src/main.cpp
@@ -0,0 +1,203 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       Student                                                   */
+/*    Created:      8/13/2026, 4:51:39 PM                                     */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+#include "vex.h"
+
+using namespace vex;
+
+// A global instance of competition
+competition Competition;
+brain Brain;
+// define your global instances of motors and other devices here
+motor leftMotor = motor(PORT1, ratio18_1, false);
+motor rightMotor = motor(PORT1, ratio18_1, false);
+motor bleftMotor = motor(PORT1, ratio18_1, false);
+motor brightMotor = motor(PORT1, ratio18_1, false);
+controller Potato_Controller = controller(primary);
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the V5 has been powered on and        */
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+void Drive(float lspeed, float rspeed, int wt){
+  leftMotor.spin(forward, lspeed, percent);
+  bleftMotor.spin(forward, lspeed, percent);
+  rightMotor.spin(forward, rspeed, percent);
+  brightMotor.spin(forward, rspeed, percent);
+  wait(wt, msec);
+}
+
+void Brake(){
+  leftMotor.stop(brake);
+  bleftMotor.stop(brake);
+  rightMotor.stop(brake);
+  brightMotor.stop(brake);
+}
+void pre_auton(void) {
+
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+}
+double YOFFSET = 20; //offset for the display
+//Writes a line for the diagnostics of a motor on the Brain
+void MotorDisplay(double y, double curr, double temp)
+{
+	Brain.Screen.setFillColor(transparent);
+	Brain.Screen.printAt(5, YOFFSET + y, "Current: %.1fA", curr);
+	
+	if (curr < 1){
+		Brain.Screen.setFillColor(green);
+	} else if(curr >= 1 && curr  <= 2.5) {
+		Brain.Screen.setFillColor(yellow);
+	} else {
+		Brain.Screen.setFillColor(red);
+		Brain.Screen.drawRectangle(140, YOFFSET + y - 15, 15, 15);
+	}
+
+	
+	Brain.Screen.setFillColor(transparent);
+	Brain.Screen.printAt(160, YOFFSET + y, "Temp: %.1fC", temp);
+	
+	if (temp < 45){
+		Brain.Screen.setFillColor(green);
+	} else if(temp <= 50 && temp  >= 45){
+		// TRUE and TRUE --> True
+		// TRUE and FALSE --> False
+		// FALSE and FALSE --> False
+		Brain.Screen.setFillColor(yellow);
+	} else {
+		Brain.Screen.setFillColor(red);
+		Brain.Screen.drawRectangle(275, YOFFSET + y - 15, 15, 15);
+		Brain.Screen.setFillColor(transparent);
+	}
+}
+
+
+//Displays information on the brain
+void Display()
+{
+	double leftFrontCurr = leftMotor.current(amp);
+	double leftFrontTemp = leftMotor.temperature(celsius);
+	double leftBackCurr = bleftMotor.current(amp);
+	double leftBackTemp = bleftMotor.temperature(celsius);
+	double rightFrontCurr = rightMotor.current(amp);
+	double rightFrontTemp = rightMotor.temperature(celsius);
+	double rightBackCurr = brightMotor.current(amp);
+	double rightBackTemp = brightMotor.temperature(celsius);
+
+
+	if (leftMotor.installed()){
+		MotorDisplay(1, leftFrontCurr, leftFrontTemp);
+		Brain.Screen.printAt(300, YOFFSET + 1, "LeftFront");
+	} else {
+		Brain.Screen.printAt(5, YOFFSET + 1, "LeftFront Problem");
+	}
+	
+	
+	if (bleftMotor.installed()){
+		MotorDisplay(31, leftBackCurr, leftBackTemp);
+		Brain.Screen.printAt(300, YOFFSET + 31, "LeftBack");
+	} else {
+		Brain.Screen.printAt(5, YOFFSET + 31, "LeftBack Problem");
+	}
+
+
+	if (rightMotor.installed()) {
+		MotorDisplay(61, rightFrontCurr, rightFrontTemp);
+		Brain.Screen.printAt(300, YOFFSET + 61, "RightFront");
+	} else {
+		Brain.Screen.printAt(5, YOFFSET + 61, "RightFront Problem");
+	}
+	
+	
+	if (brightMotor.installed()) {
+		MotorDisplay(91, rightBackCurr, rightBackTemp);
+		Brain.Screen.printAt(300, YOFFSET + 91, "RightBack");
+	} else {
+		Brain.Screen.printAt(5, YOFFSET + 91, "RightBack Problem");
+	}
+
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void autonomous(void) {
+  // ..........................................................................
+  // Insert autonomous user code here.
+  // ..........................................................................
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+void usercontrol(void) {
+  // User control code here, inside the loop
+  while (1) {
+    float prct = 1;
+    float leftSpeed = Potato_Controller.Axis3.position();
+    float rightSpeed = Potato_Controller.Axis2.position();
+    if(Potato_Controller.ButtonA.pressing()){
+      prct = 0.5;
+    }
+    else if(Potato_Controller.ButtonB.pressing()){
+      prct = 1;
+    }
+    Drive(leftSpeed*prct, rightSpeed*prct, 10);
+    Display();
+    if (Potato_Controller.ButtonX.pressing()) {
+          Brake();
+    }
+    // This is the main execution loop for the user control program.
+    // Each time through the loop your program should update motor + servo
+    // values based on feedback from the joysticks.
+
+    // ........................................................................
+    // Insert user code here. This is where you use the joystick values to
+    // update your motors, etc.
+    // ........................................................................
+    
+    wait(20, msec); // Sleep the task for a short amount of time to
+                    // prevent wasted resources.
+  }
+}
+
+//
+// Main will set up the competition functions and callbacks.
+//
+int main() {
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  // Run the pre-autonomous function.
+  pre_auton();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) {
+    wait(100, msec);
+  }
+}
